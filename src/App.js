7815dcce8c967204e5650ed99b857f03f4b2e4ef
melkamu372 <melkamu372@gmail.com>
2024-07-31 16:24:12 +0300

    addd assets

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//router
+import LayoutsRoute from './router/layouts-route';
+
+//scss files
+import './assets/scss/backend.scss';
+import  './assets/css/custom.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LayoutsRoute />
+    </div>
+  );
+}
+
+export default App;
